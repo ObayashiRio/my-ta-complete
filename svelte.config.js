2929d85bec8e5e8e69040f271a179b75f589a76b
ObayashiRio <131227219+ObayashiRio@users.noreply.github.com>
2024-12-10 18:54:04 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-vercel';
+// Vercel用のアダプターをインポート
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x'  // 必要なら 'nodejs20.x'
+    // 使用するアダプターを1つだけ指定
+    adapter: vercel({
+      runtime: 'nodejs18.x'  // 必要に応じて変更
     }),
     alias: {
       $components: './src/components',
